e78472a5f793f644a5eae955049e3456f2a088aa
TKZ700 <t138806k@gmail.com>
2025-07-28 15:08:42 +0330

    security bug fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,9 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: "0.0.0.0", // این خط را اضافه کنید
-    port: 5173, // پورت پیش‌فرض Vite است، می‌توانید تغییر دهید
-  },
+  plugins: [react()]
 });
